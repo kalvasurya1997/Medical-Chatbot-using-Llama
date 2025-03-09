9964c145038ced3cd6f6ff3ed6a448e3b3071511
surya Kalva <kalvasurya1997@gmail.com>
2025-03-08 23:12:47 -0500

    setup file edit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical ChatBot',
+    version= '0.0.0',
+    author= 'Surya Kalva',
+    author_email= 'kalvasurya1997@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
